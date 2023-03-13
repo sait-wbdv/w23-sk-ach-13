95c0faade14d00d0713328bdd2bec0e725a4189a
Tony Grimes <acidtone@tonygrimes.com>
2023-03-13 13:12:30 -0600

    changed page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,8 +23,8 @@
   }
 </script>
 
-<h1>Welcome to SvelteKit!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Tony's Loop exercise spoilers!</h1>
+
 <h2>Exercise 1</h2>
 <ul>
   {#each arrReverse as item}
